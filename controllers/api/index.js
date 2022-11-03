e543bda4f61e826dd28cf1778048e485c079b83e
Anisha Sapkota <anisha.sapkota79@gmail.com>
2022-11-03 22:20:19 +1100

    added booking and user apis

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,8 @@
-// TODO
+const router = require('express').Router();
+const bookingRoutes = require('./booking-routes');
+const userRoutes = require('./user-routes')
+
+router.use('/bookings', bookingRoutes);
+router.use('/users',userRoutes)
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 // Dependencies
-const express = require('express');
-const exphbs = require('express-handlebars');
-const path = require('path');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
 const hbs = exphbs.create({});
-
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
 
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set Handlebars as the default template engine.
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/index.js'));
+// Set Handlebars as the default template engine.
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // Starts the server to begin listening
-app.listen(PORT, () => {
-  console.log('Server listening on: http://localhost:' + PORT);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
 });

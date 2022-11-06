4fa0187ca4a9bdcbbf5be380ce5507693009faa2
Anisha Sapkota <anisha.sapkota79@gmail.com>
2022-11-06 11:22:30 +1100

    added login and logout api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,12 @@
-const router = require('express').Router();
-const bookingRoutes = require('./booking-routes');
-const userRoutes = require('./user-routes')
+const router = require("express").Router();
+const bookingRoutes = require("./booking-routes");
+const userRoutes = require("./user-routes");
+const loginRoute = require("./login-route");
+const logoutRoute = require("./logout-route");
 
-router.use('/bookings', bookingRoutes);
-router.use('/users',userRoutes)
+router.use("/bookings", bookingRoutes);
+router.use("/users", userRoutes);
+router.use("/login", loginRoute);
+router.use("/logout", logoutRoute);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 // Dependencies
 const express = require("express");
+const session = require("express-session");
+const { v4: uuidv4 } = require("uuid");
 const exphbs = require("express-handlebars");
 const path = require("path");
 const hbs = exphbs.create({});
 const routes = require("./controllers");
+
 const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// session
+const sess = {
+  secret: uuidv4(),
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
